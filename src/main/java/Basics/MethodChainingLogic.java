d7e0314664104682e9bfdcfee51e990c865893bf
Nithesh <nitheshkumar0731@gmail.com>
2023-01-28 14:43:20 +0530

    Method Chaining concepts Implemented

--- src/main/java/Basics/MethodChainingLogic.java
+++ src/main/java/Basics/MethodChainingLogic.java
@@ -1,5 +1,17 @@
 package Basics;
 
 public class MethodChainingLogic {
+public  MethodChainingLogic returnObject(){
+    System.out.println("i am return Object for chaining");
+    return  this;
+}
+public  MethodChainingLogic returnChainedMethod(){
+    System.out.println("i am  returnChainedMethod");
+    return  this;
+}
+
+    public static void main(String[] args) {
+      new MethodChainingLogic().returnObject().returnChainedMethod();
+    }
 
 }
