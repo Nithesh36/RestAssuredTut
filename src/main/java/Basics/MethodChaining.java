05ef219a2e780bb51a3485fe6228b39597d9f92a
Nithesh <nitheshkumar0731@gmail.com>
2023-01-28 14:37:32 +0530

    Method Chaining concepts

--- src/main/java/Basics/MethodChaining.java
+++ src/main/java/Basics/MethodChaining.java
@@ -1,8 +1,19 @@
 package Basics;
 
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class MethodChaining {
     public static void main(String[] args) {
-        //empfff
+        List<String> names = Arrays.asList("apple", "mango");
+       // Stream<String> nameStream = names.stream();
+        //Stream<String> updatedName = nameStream.map(e -> e + " Fruits");
+        System.out.println("new Names:");
+        //System.out.println(updatedName.collect(Collectors.toList()));
+        // --multiple methods are chained is   method chaining - note below code
+        List<String> nameStream = names.stream().map(e -> e + " Fruits").collect(Collectors.toList());
     }
 }
