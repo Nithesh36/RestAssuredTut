4c8d692f9430b13f9a8611dd567f3c80bf7b9672
Nithesh <nitheshkumar0731@gmail.com>
2023-01-28 12:50:03 +0530

    Method chaining

---
+++ src/main/java/org/automation/App.java
@@ -0,0 +1,22 @@
+package org.automation;
+import static java.lang.System.out;
+import  static  java.lang.System.out;
+/**
+ * Hello world!
+ *
+ */
+public class App
+
+{
+    public static void setDummy(int dummy1) {
+        dummy = dummy1;
+    }
+
+    public static int  dummy=5;
+    public static void main( String[] args )
+    {
+
+        out.println( "Hello World!" );
+
+    }
+}
